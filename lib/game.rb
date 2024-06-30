5b81e7f21af0a7d461108e6e45c2457fc2e6b6bb
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-06-30 15:08:17 -0700

    Cleaned up comments

--- lib/game.rb
+++ lib/game.rb
@@ -17,8 +17,6 @@ class Game
     @lifes = 10
   end
 
-  # Solution doesn't work because it returns only first match, even if guess is included in solution multiple times
-  # However, I think direction is right.
   def check_guess(guess)
     if guess.size == 1 && solution.include?(guess)
       solution.each_char.with_index { |letter, idx| wordboard[idx] = guess if letter.eql?(guess) }
@@ -30,13 +28,11 @@ class Game
     end
   end
 
-  # works currently only with single letter guess. Implement whole word solve later
   def play_round
     puts lifes > 1 ? game_message('lifes_left') : game_message('last_try')
     last_guess = player.getting_guess
     check_guess(last_guess)
     display_board(wordboard)
-    # @lifes -= 1
   end
 
   def run_full_game
@@ -55,11 +51,8 @@ class Game
     Array.new(word.size, '_')
   end
 
-  # Maybe split board display and letters left into seperate methods. Just storing here.
   def display_board(array)
     puts "\n\n#{array.join(' ')}   #{array.count('_')} letters left\n\n"
     puts "Already guessed: #{wrong_guesses.join(', ')}"
   end
-
-  # Should Display class be created?
 end
