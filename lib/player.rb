6a4243d83a0f1362f5a1a8e7b1f3ed67209f7cfb
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-06-30 12:20:47 -0700

    Created method to check guess format and implemented Notificationable module

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,18 @@
+require './lib/notifications'
+
 # Player Class
 class Player
+  include Notificationable
+
   def getting_guess
-    print 'Type in your guess: '
-    gets.chomp.downcase
+    print prompt_message('guess_prompt')
+    validate_guess(gets.chomp.to_s.downcase)
+  end
+
+  def validate_guess(guess)
+    return guess if guess.between?('a', 'z') && guess.size.eql?(1) || guess == 'solve'
+
+    puts prompt_message('guess_error')
+    getting_guess
   end
 end
