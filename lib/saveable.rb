fa03ced00c9a9f4419a54c48213260e9ee9292c2
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-07-04 13:39:06 -0700

    Re-ordered code

--- lib/saveable.rb
+++ lib/saveable.rb
@@ -3,18 +3,6 @@ require './lib/messageable'
 # Saveable Module for save / load function of game progress
 module Saveable
   include Messageable
-  # New save game feature. It works, but I feel code could be more elegant. Also better in own module/class
-  def save_game(game)
-    # save = game.to_yam
-    filename = 'save_game/save_file.yaml'
-
-    File.open(filename, 'w') do |file|
-      file.puts game.to_yaml
-    end
-    puts prompt_message('game_saved')
-    exit
-  end
-
   def self.load_game
     # save_file = File.open 'save_game/save_file.yaml'
     save_game = YAML.load_file('save_game/save_file.yaml', permitted_classes: [Game, Player])
@@ -30,5 +18,18 @@ module Saveable
     else
       Game.new.run_full_game
     end
+  end
+
+  private
+
+  def save_game(game)
+    # save = game.to_yam
+    filename = 'save_game/save_file.yaml'
+
+    File.open(filename, 'w') do |file|
+      file.puts game.to_yaml
+    end
+    puts prompt_message('game_saved')
+    exit
   end
 end
