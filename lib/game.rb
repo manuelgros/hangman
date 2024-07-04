8db184c382582bc28d843ecf6f59cd4f0570b3bb
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-07-04 13:11:18 -0700

    Moved save/load feature into its own module and adjusted code accordingly

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,14 @@
 require './lib/player'
 require './lib/messageable'
 require './lib/game_logic'
+require './lib/saveable'
 require 'pry-byebug'
 
 # Game class
 class Game
   include Messageable
   include GameLogic
+  include Saveable
 
   attr_reader :solution, :player
   attr_accessor :last_guess, :lifes, :wordboard, :wrong_guesses
@@ -20,40 +22,13 @@ class Game
     @lifes = 10
   end
 
-  # New save game feature. It works, but I feel code could be more elegant. Also better in own module/class
-  def save_game
-    save = to_yaml
-    filename = 'save_game/save_file.yaml'
-
-    File.open(filename, 'w') do |file|
-      file.puts save
-    end
-    puts game_message('game_saved')
-    exit
-  end
-
-  def self.load_game
-    save_file = File.open 'save_game/save_file.yaml'
-    save_game = YAML.load_file(save_file, permitted_classes: [Game, Player])
-    save_file.close
-    File.delete(save_file)
-    save_game.run_full_game
-  end
-
-  def self.want_to_load
-    load_game if gets.chomp == 'y'
-    display_board(wordboard)
-  end
-
   def self.star_game
     if File.exist? 'save_game/save_file.yaml'
-      puts 'Save game exists. Do you want to continue? '
-      want_to_load
+      Saveable.want_to_load
     else
       Game.new.run_full_game
     end
   end
-  # End of save game feature
 
   def run_full_game
     puts game_message('game_starts')
