4983a694613752d3eccff6abc17ec8a0da3fb7b4
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-07-04 13:10:22 -0700

    Minor changes

--- lib/messageable.rb
+++ lib/messageable.rb
@@ -9,9 +9,7 @@ module Messageable
       'last_try' => 'ONLY 1 Life LEFT!'.red,
       'solution' => "The solution was #{solution.upcase}",
       'player_won' => "You got it! Solution: #{solution.upcase}.  Lifes left: #{lifes}. Great job!".green,
-      'player_lost' => "You are out of lifes. The solution was #{solution.upcase}. You'l get it next time!".red,
-      'game_saved' => 'Your game has been saved. See you next time, bye!'
-      
+      'player_lost' => "You are out of lifes. The solution was #{solution.upcase}. You'l get it next time!".red
     }[message]
   end
 
@@ -21,7 +19,7 @@ module Messageable
       'again' => 'Do you want to play again? Type Y for yes: ',
       'error' => "\nUnvalid guess! Please make sure you only type either a single letter of the english alphabet
 or a full word if you wish to guess solve.\n".yellow,
-      'load_game' => 'There is a saved game. Do you want to continue? Y for yes'
+      'game_saved' => 'Your game has been saved. See you next time, bye!'
     }[message]
   end
 end
