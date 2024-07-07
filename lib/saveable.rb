eeb8b80b582a2bf81510155121acd0925de0364b
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-07-07 15:14:43 -0700

    Debugging

--- lib/saveable.rb
+++ lib/saveable.rb
@@ -1,10 +1,13 @@
 require './lib/messageable'
+require 'yaml'
 
 # Saveable Module for save / load function of game progress
 module Saveable
   include Messageable
 
   def want_to_load
+    return Game.new.run_full_game if create_save_slots[1..].all? { |file| File.empty?(file) }
+
     puts 'Do you want to load an exisiting game or start new? L for load / N for new'
     if gets.chomp == 'l'
       select_save_game
@@ -16,8 +19,8 @@ module Saveable
   private
 
   def create_save_slots
-    slot2 = File.open('./save_game/save_slot_2.yaml', 'r')
     slot1 = File.open('./save_game/save_slot_1.yaml', 'r')
+    slot2 = File.open('./save_game/save_slot_2.yaml', 'r')
     slot3 = File.open('./save_game/save_slot_3.yaml', 'r')
     ['', slot1, slot2, slot3]
   end
@@ -29,7 +32,7 @@ module Saveable
 
   def select_save_game
     display_slots(create_save_slots)
-    print 'Choose save game (1-3):'
+    print 'Choose save game (1-3) or type N for new game:'
     selection = gets.chomp.to_i
     if selection.between?(1, 3) && !File.empty?(create_save_slots[selection])
       load_game(create_save_slots[selection])
@@ -40,7 +43,7 @@ module Saveable
   end
 
   def save_game(game, slots_array)
-    puts 'Select slot to save curretn game'
+    puts 'Select slot to save current game'
     display_slots(slots_array)
     print 'Select slot number: '
 
